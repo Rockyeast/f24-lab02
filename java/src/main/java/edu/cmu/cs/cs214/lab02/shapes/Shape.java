4934a449d147158064accc178d4ebaaaf74a0401
Rockyeast <zhifengl@andrew.cmu.edu>
2024-09-06 13:28:03 -0400

    Refactor: set shape attributes to private and implement Shape interface for encapsulation

--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Shape.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Shape.java
@@ -1,5 +1,12 @@
 package edu.cmu.cs.cs214.lab02.shapes;
 
-public interface Shape {
-    
+public class Square implements Shape {
+    private double sideLen;  
+    public Square(double sideLen) {
+        this.sideLen = sideLen;
+    }
+
+    public double getArea() {
+        return sideLen * sideLen;
+    }
 }
