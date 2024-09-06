5d5915ed82fc4296b545242257ac99b3c8e15c1d
Rockyeast <zhifengl@andrew.cmu.edu>
2024-09-06 13:30:37 -0400

    Refactor: use Shape interface in Main to decouple from specific Rectangle implementation

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,14 @@
 package edu.cmu.cs.cs214.lab02;
 
-import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.*;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
-
+        Shape rectangle = new Rectangle(2, 3);  // 使用 Shape 接口
+        
         Renderer renderer = new Renderer(rectangle);
-
+        
         renderer.draw();
     }
 }
+
